d7ea401cf1b4dbc9e75ec98a92cace70d10daca0
Fred Woolard <woolardfa@appstate.edu>
2013-02-20 15:35:54 -0500

    Additional properties for $plugin, add README.txt

--- version.php
+++ version.php
@@ -30,5 +30,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013021401;       // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012062501;       // Requires this Moodle version
+$plugin->cron      = 0;
 $plugin->component = 'filter_rtmp';    // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0 (Build: 2013022000)';
 
