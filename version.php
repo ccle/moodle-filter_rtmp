5b615d5041afc13780cf5d84635566aff090959c
Fred Woolard <woolardfa@appstate.edu>
2013-02-27 18:01:02 -0500

    Adjust for Cloudfront difference in netConnectionUrl

--- version.php
+++ version.php
@@ -28,10 +28,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013052001;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013052002;       // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012120301;       // Requires this Moodle version
 $plugin->cron      = 0;
 $plugin->component = 'filter_rtmp';    // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0 (Build: 2013022000)';
+$plugin->release   = '1.0 (Build: 2013022800)';
 
